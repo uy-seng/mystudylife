caa2737f3b4cc0ca600e4d8e348170d78d3e6f88
nova44056 <sengouy0@gmail.com>
2021-11-08 10:07:56 +0700

    update database service

--- src/services/database.service.ts
+++ src/services/database.service.ts
@@ -29,7 +29,7 @@ export class DatabaseService {
   }
 
   public async init() {
-    this.connection = await createConnection("production");
+    this.connection = await createConnection(`${process.env.NODE_ENV}`);
     console.log("Database initialized...");
     const CLEAN_DB = false;
     /**
