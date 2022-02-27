09e618ccd5b2517bfe581bcb263bf664ae563823
Uy Seng <nova@Uys-MacBook-Air.local>
2022-02-27 16:08:18 -0600

    update oauth facebook build

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,7 @@
 "use strict";
-var __importDefault =
-  (this && this.__importDefault) ||
-  function (mod) {
-    return mod && mod.__esModule ? mod : { default: mod };
-  };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 require("reflect-metadata");
 const express_1 = __importDefault(require("express"));
@@ -18,56 +16,55 @@ const resolvers_1 = require("./graphql/resolvers");
 const services_1 = require("./services");
 const path_1 = __importDefault(require("path"));
 (async () => {
-  const app = (0, express_1.default)();
-  app.use(body_parser_1.default.urlencoded({ extended: true }));
-  app.use(express_1.default.json());
-  app.use((0, cookie_parser_1.default)());
-  app.use(
-    (0, cors_1.default)({
-      credentials: true,
-      origin: ["https://mystudylife-clone-useng.herokuapp.com"],
-    })
-  );
-  const databaseService = new services_1.DatabaseService();
-  await databaseService.init();
-  const passportService = new services_1.PassportService(app);
-  passportService.initGoogleOAuthStrategy();
-  passportService.initFacebookOAuthStrategy();
-  app.use("/oauth", oauth_route_1.oauthRoute);
-  app.use("/api", routes_1.apiRoute);
-  const apolloServer = new apollo_server_express_1.ApolloServer({
-    schema: await (0, type_graphql_1.buildSchema)({
-      resolvers: [
-        resolvers_1.AuthResolver,
-        resolvers_1.AcademicYearResolver,
-        resolvers_1.AcademicYearScheduleResolver,
-        resolvers_1.TermResolver,
-        resolvers_1.SubjectResolver,
-        resolvers_1.ClassResolver,
-        resolvers_1.ClassScheduleResolver,
-        resolvers_1.OneOffScheduleResolver,
-        resolvers_1.RepeatScheduleResolver,
-        resolvers_1.TaskResolver,
-        resolvers_1.HolidayResolver,
-      ],
-    }),
-    context: ({ req, res }) => ({ req, res }),
-  });
-  await apolloServer.start();
-  apolloServer.applyMiddleware({ app, cors: false });
-  const PORT = process.env.PORT || 8000;
-  if (process.env.NODE_ENV === "production") {
-    app.use(express_1.default.static("web/build"));
-    app.get("*", (_req, res) => {
-      return res.sendFile(
-        path_1.default.resolve(__dirname, "web", "build", "index.html")
-      );
+    const app = (0, express_1.default)();
+    app.use(body_parser_1.default.urlencoded({ extended: true }));
+    app.use(express_1.default.json());
+    app.use((0, cookie_parser_1.default)());
+    app.use((0, cors_1.default)({
+        credentials: true,
+        origin: [
+            "https://studio.apollographql.com",
+            "http://localhost:3000",
+            "http://localhost:3001",
+        ],
+    }));
+    const databaseService = new services_1.DatabaseService();
+    await databaseService.init();
+    const passportService = new services_1.PassportService(app);
+    passportService.initGoogleOAuthStrategy();
+    passportService.initFacebookOAuthStrategy();
+    app.use("/oauth", oauth_route_1.oauthRoute);
+    app.use("/api", routes_1.apiRoute);
+    const apolloServer = new apollo_server_express_1.ApolloServer({
+        schema: await (0, type_graphql_1.buildSchema)({
+            resolvers: [
+                resolvers_1.AuthResolver,
+                resolvers_1.AcademicYearResolver,
+                resolvers_1.AcademicYearScheduleResolver,
+                resolvers_1.TermResolver,
+                resolvers_1.SubjectResolver,
+                resolvers_1.ClassResolver,
+                resolvers_1.ClassScheduleResolver,
+                resolvers_1.OneOffScheduleResolver,
+                resolvers_1.RepeatScheduleResolver,
+                resolvers_1.TaskResolver,
+                resolvers_1.HolidayResolver,
+            ],
+        }),
+        context: ({ req, res }) => ({ req, res }),
     });
-  }
-  app.listen(PORT, () => {
-    console.log(
-      "Client running at https://mystudylife-clone-useng.herokuapp.com/"
-    );
-  });
+    await apolloServer.start();
+    apolloServer.applyMiddleware({ app, cors: false });
+    const PORT = process.env.PORT || 8000;
+    if (process.env.NODE_ENV === "production") {
+        app.use(express_1.default.static("web/build"));
+        app.get("*", (_req, res) => {
+            return res.sendFile(path_1.default.resolve(__dirname, "web", "build", "index.html"));
+        });
+    }
+    app.listen(PORT, () => {
+        console.log("Client running at http://localhost:8000");
+        console.log(`Server running at http://localhost:8000\nGraphql running at http://localhost:8000/graphql`);
+    });
 })();
 //# sourceMappingURL=index.js.map
