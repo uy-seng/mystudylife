e232301f097f9e8830a0ce3933695be92c20466d
Uy Seng <nova@Uys-MacBook-Air.local>
2022-02-27 18:51:20 -0600

    debug google oauth bug

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Switch } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { Guest } from "./pages/Guest";
 import { Dashboard } from "./pages/Dashboard";
 import { SignUp } from "./pages/SignUp";
